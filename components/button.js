626492a440a068aa68ea65f3b6cb51e7e43f9b8e
fede <federico.hammond8@gmail.com>
2023-04-02 15:44:57 -0300

    start projec nextjs

---
+++ components/button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Button({Icon, text}) {
+  return (
+    <>
+    <button className="flex justify-center self-center border-zinc-700 border rounded-3xl w-32 pt-2 pb-2 mb-12">
+    <Icon className="flex justify-center self-center" />
+    <span className="ml-2 uppercase text-xs">{text}</span>
+    </button>
+    </>
+  )
+}
