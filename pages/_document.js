ef543065952bd44ba06bc9b7017346f8a28813e6
fd <federico.hammond@gmail.com>
2023-05-27 13:52:33 -0300

    add favicon to website

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
+          rel="shortcut icon"
+          href="https://res.cloudinary.com/dzkqopnby/image/upload/v1685206109/icon_lljf4f.png"
+        />
+        <link
           href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;700;900&display=swap"
           rel="stylesheet"></link>
         <meta
